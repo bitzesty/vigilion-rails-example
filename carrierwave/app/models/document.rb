a55c440a0597db7ad05c10a783d124d7671ab623
Emiliano Ritiro <eritiro@gmail.com>
2015-05-27 21:35:19 -0300

    moved generic stuff to rails gem

--- carrierwave/app/models/document.rb
+++ carrierwave/app/models/document.rb
@@ -2,10 +2,5 @@ require "virus_scanner"
 
 class Document < ActiveRecord::Base
   mount_uploader :attachment, AttachmentUploader
-
-  def store_attachment!
-    super()
-    response = ::VirusScanner::File.scan_url(id,
-      'http://fineprintnyc.com/images/blog/history-of-logos/google/google-logo.png')
-  end
+  scan_file :attachment
 end
