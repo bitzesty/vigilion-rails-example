d8b41ca92a442bd4cd37a1fe31132a66ccabfacf
Emiliano Ritiro <eritiro@gmail.com>
2015-06-01 22:04:05 -0300

    fixing mess

--- carrierwave/config/initializers/carrierwave.rb
+++ carrierwave/config/initializers/carrierwave.rb
@@ -2,10 +2,10 @@ CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: 'AKIAIICXHKTOE6YGANKA',
-    aws_secret_access_key: '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'
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
   }
 
-  config.fog_directory  = 'dev-vigilion-example'
+  config.fog_directory  = ENV['AWS_S3_BUCKET_NAME']
   config.fog_public     = false
 end
