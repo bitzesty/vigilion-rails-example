6f7abbd3b026e96ead01a17fa1aba25fddb7d37c
Emiliano Ritiro <eritiro@gmail.com>
2015-06-01 22:00:40 -0300

    Carrierwave using S3

--- carrierwave/db/schema.rb
+++ carrierwave/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150529135100) do
+ActiveRecord::Schema.define(version: 20150529170610) do
 
   create_table "documents", force: :cascade do |t|
     t.string   "name"
